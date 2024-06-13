19d19b7e26a3de644bad54345fa9c49d2ec007ed
Fabio William <fabiowilliamlf@gmail.com>
2024-06-13 11:06:05 -0300

    Refactor button status logic and add reset functionality

--- app.js
+++ app.js
@@ -25,10 +25,20 @@ function obterNumeroAleatorio(min, max) {
 }
 
 function alterarStatusBotao() {
-    let botao = document.getElementById('reiniciar');
-    if (botao.disabled) {
-        botao.disabled = false;
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
     } else {
-        botao.disabled = true;
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
     }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotao();
 }
